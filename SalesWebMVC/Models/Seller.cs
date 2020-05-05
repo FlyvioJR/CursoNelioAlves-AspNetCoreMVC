9ed9c276898f592754a8be23181e39b66af25df4
FlyvioJR <flaviotaylorr@gmail.com>
2020-05-05 19:06:17 -0300

    SellerFormViewModel and Department select component

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -13,7 +13,7 @@ namespace SalesWebMVC.Models
         public double BaseSalary{ get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
-        public int DepartmentiD { get; set; }
+        public int DepartmentId { get; set; }
         public Seller() { }
 
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
